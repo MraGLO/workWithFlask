0db412e71cb2679a8ed6d203130ad55d752d1dba
MraGLO <94346067+MraGLO@users.noreply.github.com>
2023-05-12 13:18:50 +0300

    Add files via upload

--- Кузичев Марк ИСП-320П/lesson10/lesson10_main2.py
+++ Кузичев Марк ИСП-320П/lesson10/lesson10_main2.py
@@ -1,34 +1,18 @@
 from flask import Flask, render_template
-from flask_script import Manager, Command, Shell
+from flask_script import Manager
 
 app = Flask(__name__)
 manager = Manager(app)
-
-class Faker(Command):
-    def run(self):
-        print("Fake data entered")
-
-Manager.add_command("faker", Faker())
-
-@manager.command
-def foo():
-    print("foo command executed")
-
-def shell_context():
-    import os, sys
-    return dict(app=app, os=os, sys=sys)
-
-manager.add_command("shell", Shell(make_context=shell_context))    
 
 @app.route('/')
 def index():
     return render_template('index.html', name='Jerry')
 
-@app.route('/user/<user_id>/')
+@app.route('/user//')
 def user_profile(user_id):
     return "Profile page of user #{}".format(user_id)
 
-@app.route('/books/<genre>/')
+@app.route('/books//')
 def books(genre):
     return "All Books in {} category".format(genre)
 
--- Кузичев Марк ИСП-320П/lesson7/lesson7_templates.py
+++ Кузичев Марк ИСП-320П/lesson7/lesson7_templates.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name, age, profession = "Jerry", 24, 'Programmer'
-    template_context = dict(name=name, age=age, profession=profession)
-    return render_template('index.html', **template_context)
+    name, fruit = "jerry", "apple"
+    x, y = 5, 6 
+    template_context = dict(name=name, fruit = fruit, x =x, y= y)
+    return render_template('lesson7_create_templates.html', **template_context)
 
 @app.route('/html')
 def index_html():
-    return render_template('index.html')
+    return render_template('lesson7_create_templates.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
